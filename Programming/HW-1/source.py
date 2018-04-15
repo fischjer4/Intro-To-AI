b11f61f8dd2e0320521188c341e1fb09f54a59ed
fischjer4 <fischjer4@gmail.com>
2018-04-15 14:50:00 -0700

    added expand, successor, and Node functionality

--- Programming/HW-1/source.py
+++ Programming/HW-1/source.py
@@ -1,5 +1,26 @@
 import sys
 
+# Global Vars
+nodesExpanded = 0
+
+
+########################################################
+#	parent: 	A Node
+#	state: 		A tuple array representing 
+# 				[leftBank, rightBank] [[2,3,1], [1,0,0]]
+#	pathCost: 	The cost to get to this node from root
+#	depth:		The depth of this node
+#	action:		The action done to generate this node
+########################################################
+class Node:
+	def __init__(self, parent, state, pathCost, depth, action):
+		self.parent = parent
+		self.state = state
+		self.pathCost = pathCost
+		self.depth = depth
+		self.action = action
+
+
 
 
 ##################################################
@@ -31,17 +52,122 @@ def executeASTAR(startFO, goalFO, outputFO):
 	pass
 
 
+#############################################################
+#	Backtracks from the GOAL node to the ROOT node pushing the 
+# 	reversed solution path along onto a stack. Then, prints 
+# 	the soln by poping stack
+#############################################################
+def solution(node, outputFO):
+	solutionNodes = 0
+	curNode = node
+	stack = list()
+	# Backtrack and push steps to stack
+	while curNode:
+		step = ''.join(curNode.state[0]) + ',' + ''.join(curNode.state[1])
+		stack.append(step)
+		curNode = curNode.parent
+		solutionNodes += 1
+
+	# Print path from root to goal
+	while stack:
+		print stack.pop()
+
+	global nodesExpanded
+	outputFO.write("nodes expanded: " + str(nodesExpanded))
+	outputFO.write("nodes in solution: " + str(solutionNodes))
+
+
+#############################################################
+#	Given an action and a state the function returns
+#	None if the action results in an invalid state. Otherwise
+#	the new state is returned.
+#############################################################
+def computeNextState(action, state):
+	# if the boat is on the left bank
+	if state[0][2]:
+		newState = [[state[0][i] - action[i] for i in range(2)], [state[1][i] + action[i] for i in range(2)]]
+		newState[0].append(0)	
+		newState[1].append(1)
+	# if the boat is on the right bank
+	else:
+		newState = [[state[0][i] + action[i] for i in range(2)], [state[1][i] - action[i] for i in range(2)]]
+		newState[0].append(1)	
+		newState[1].append(0)
+
+	# Verify a valid state. i.e. If there are chickens on the bank. 
+	# The number of wolves must be  < the number of chickens
+	if (state[0][1] > 0 and state[0][1] > state[0][0]) or (state[1][1] > 1 and state[1][1] > state[1][0]):
+		return None
+	# verify positive values
+	elif state[0][0] < 0 or state[0][1] < 0 or state[1][0] < 0 or state[1][1] < 0:
+		return None
+
+	# Good state. Return it
+	return newState
+	
+
+
+#############################################################
+#	Given a node, a list containing [action, result] pairs
+#	is returned where result is the new state after the action
+# 	is applied to the node's current state.
+#############################################################
+def successorFN(node):
+	# [chickens, wolves]
+	actions = [[1,0],[0,1],[1,1],[2,0],[0,2]]
+	state = node.state
+
+	actionResultPairs = []
+	for action in actions:
+		newState = computeNextState(action, state)
+		if newState:
+			actionResultPairs.append([action, newState])
+
+	return actionResultPairs
+
+
+#################################################
+#	Expands the given node
+#################################################
+def expand(node):
+	successors = []
+	for action, result in successorFN(node):
+		# pathcost of new node may need to be (node.pathCost + StepCost(newNode))
+		# where StepCost() evaluates the cost. i.e. if the cost isn't uniformly 1
+		newNode = Node(node, result, node.pathCost + 1, node.depth + 1, action)
+		successors.append(newNode)
+
+	return successors
+
+#############################################################
+# The start and goal files are in the following order:
+# 	Left Bank: [chickens, wolves, boat] where boat is a 1 or 0
+# 			   representing is on that bank or not
+# 	Right Bank:[chickens, wolves, boat] where boat is a 1 or 0 
+# 			   representing is on that bank or not
+#############################################################
 def main():
 	# Get cmmd line args
 	args = sys.argv
 	if len(args) is not 5:
 		print("Lacking four cmmd line args: < initial state file > < goal state file > < mode > < output file >")
 		return
+	
 
 	# Open files
 	try:
+		# Start state. e.g [['3','3','1'], [ '0','0','0']]		
 		startFO = open(args[1], "r")
+		startLeftBank = startFO.readline()
+		startRightBank = startFO.readline()
+		startState = [[int(e) for e in startLeftBank.replace(',','')], [int(i) for i in startRightBank.replace(',','')]]
+
+		# Goal state. e.g [[ '0','0','0'], ['3','3','1']]
 		goalFO = open(args[2], "r")
+		goalLeftBank = goalFO.readline()
+		goalRightBank = goalFO.readline()
+		goalState = [[int(e) for e in goalLeftBank.replace(',','')], [int(i) for i in goalRightBank.replace(',','')]]
+
 		mode = args[3]
 		outputFO = open(args[4], "w")
 	except:
