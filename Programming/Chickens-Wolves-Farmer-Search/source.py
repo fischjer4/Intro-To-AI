49fb46e032648454377e4fad3aa33f7280c235c9
fischjer4 <fischjer4@gmail.com>
2018-05-24 13:14:44 -0700

    Added vocabulary generation and feature vector generation

--- Programming/Chickens-Wolves-Farmer-Search/source.py
+++ Programming/Chickens-Wolves-Farmer-Search/source.py
@@ -9,7 +9,7 @@ astarMaxPath  = 1000
 
 ########################################################
 #	parent: 	A Node
-#	state: 		A tuple array representing 
+#	state: 		A tuple array representing
 # 				[leftBank, rightBank] [[2,3,1], [1,0,0]]
 #	pathCost: 	The cost to get to this node from root
 #	depth:		The depth of this node
@@ -24,9 +24,9 @@ class Node:
 		self.action = action
 
 ########################################################
-#	A Min heap/Priority Queue that is implemented using 
-# 	heapq. The class's push function takes in a Node and 
-# 	uses the Node's pathcost as the value. Meaning, the 
+#	A Min heap/Priority Queue that is implemented using
+# 	heapq. The class's push function takes in a Node and
+# 	uses the Node's pathcost as the value. Meaning, the
 # 	Nodes position in the PQ is dependent on the pathCost
 #	SOURCE: https://stackoverflow.com/a/8875823/6505563
 ########################################################
@@ -40,10 +40,10 @@ class PriorityQueue(object):
 			self._data = []
 	def push(self, node):
 		heapq.heappush(self._data, (self.key(node.pathCost), node))
-	
+
 	def pop(self):
 		return heapq.heappop(self._data)[1]
-	
+
 	def empty(self):
 		return len(self._data) > 0
 
@@ -55,8 +55,8 @@ def printF(f):
 	while a:
 		print a.pop().state
 	print
-	print 
-	
+	print
+
 ##################################################
 #	Attempts to solve the problem using BFS search
 #	Returns True if solution found
@@ -66,7 +66,7 @@ def executeBFS(root, goalState, outputFO):
 	closed = []
 	fringe = []
 	fringe.append(root)
-	
+
 	while True:
 		# if fringe is empty
 		if not fringe:
@@ -89,7 +89,7 @@ def executeDFS(root, goalState, outputFO):
 	closed = []
 	fringe = []
 	fringe.append(root)
-	
+
 	while True:
 		# if fringe is empty
 		if not fringe:
@@ -117,7 +117,7 @@ def executeIDDFS(root, goalState, outputFO):
 	while True:
 		closed = []
 		fringe = []
-		fringe.append(root)		
+		fringe.append(root)
 
 		while True:
 			# if fringe is empty
@@ -144,13 +144,13 @@ def executeIDDFS(root, goalState, outputFO):
 
 #################################################
 #	Heuristic function for  A* search
-# 	Remove condition of #Chickens >= #Wolves on bank, 
-# 	and then best possible way to move all over to 
+# 	Remove condition of #Chickens >= #Wolves on bank,
+# 	and then best possible way to move all over to
 # 	other bank, with having to bring an animal back is
 #
 # 	2,2,1.     1,1,0
 # 	N = Total animals on starting bank
-# 	H(n) = 2*(n-2) + 1 = 2*n - 3	
+# 	H(n) = 2*(n-2) + 1 = 2*n - 3
 #################################################
 
 def heuristic(state):
@@ -172,7 +172,7 @@ def executeASTAR(root, goalState, outputFO):
 	while True:
 		closed = []
 		fringe = PriorityQueue()
-		fringe.push(root)		
+		fringe.push(root)
 
 		while True:
 			# if fringe is empty
@@ -200,8 +200,8 @@ def executeASTAR(root, goalState, outputFO):
 
 
 #############################################################
-#	Backtracks from the GOAL node to the ROOT node pushing the 
-# 	reversed solution path along onto a stack. Then, prints 
+#	Backtracks from the GOAL node to the ROOT node pushing the
+# 	reversed solution path along onto a stack. Then, prints
 # 	the soln by poping stack
 #############################################################
 def solution(node, outputFO):
@@ -210,7 +210,7 @@ def solution(node, outputFO):
 	stack = list()
 	# Backtrack and push steps to stack
 	while curNode:
-		step = ''.join(str(e) for e in curNode.state[0]) + ',' + ''.join(str(e) for e in curNode.state[1]) 
+		step = ''.join(str(e) for e in curNode.state[0]) + ',' + ''.join(str(e) for e in curNode.state[1])
 		stack.append(step)
 		curNode = curNode.parent
 		solutionNodes += 1
@@ -241,26 +241,26 @@ def computeNextState(action, state):
 	# if the boat is on the left bank
 	if state[0][2]:
 		newState = [[state[0][i] - action[i] for i in range(2)], [state[1][i] + action[i] for i in range(2)]]
-		newState[0].append(0)	
+		newState[0].append(0)
 		newState[1].append(1)
 	# if the boat is on the right bank
 	else:
 		newState = [[state[0][i] + action[i] for i in range(2)], [state[1][i] - action[i] for i in range(2)]]
-		newState[0].append(1)	
+		newState[0].append(1)
 		newState[1].append(0)
 
 	# verify positive values
 	if any(n < 0 for n in newState[0]) or any(n < 0 for n in newState[1]):
 		return None
 
-	# Verify a valid state. i.e. If there are chickens on the bank. 
+	# Verify a valid state. i.e. If there are chickens on the bank.
 	# The number of wolves must be  < the number of chickens
 	if (newState[0][0] > 0 and newState[0][1] > newState[0][0]) or (newState[1][0] > 0 and newState[1][1] > newState[1][0]):
 		return None
-	
+
 	# Good state. Return it
 	return newState
-	
+
 
 
 #############################################################
@@ -304,7 +304,7 @@ def expand(node):
 # The start and goal files are in the following order:
 # 	Left Bank: [chickens, wolves, boat] where boat is a 1 or 0
 # 			   representing is on that bank or not
-# 	Right Bank:[chickens, wolves, boat] where boat is a 1 or 0 
+# 	Right Bank:[chickens, wolves, boat] where boat is a 1 or 0
 # 			   representing is on that bank or not
 #############################################################
 def main():
@@ -313,32 +313,31 @@ def main():
 	if len(args) is not 5:
 		print("Lacking four cmmd line args: < initial state file > < goal state file > < mode > < output file >")
 		return
-	
+
 
 	# Open files
 	try:
-		# Start state. e.g [[3, 3, 1], [0, 0, 0]]		
+		# Start state. e.g [[3, 3, 1], [0, 0, 0]]
 		startFO = open(args[1], "r")
 		startLeftBank = startFO.readline().strip('\n')
 		startRightBank = startFO.readline().strip('\n')
 		startState = [[int(e) for e in startLeftBank.split(',')], [int(i) for i in startRightBank.split(',')]]
 		root = Node(None, startState, heuristic(startState), 0, None)
 		startFO.close()
-		
+
 		# Goal state. e.g [[0, 0, 0], [3, 3, 1]]
 		goalFO = open(args[2], "r")
 		goalLeftBank = goalFO.readline().strip('\n')
 		goalRightBank = goalFO.readline().strip('\n')
 		goalState = [[int(e) for e in goalLeftBank.split(',')], [int(i) for i in goalRightBank.split(',')]]
 		goalFO.close()
-		
+
 		mode = args[3]
 		outputFO = open(args[4], "w")
-	except e:
-		print e
+	except:
 		print("Error on opening a file. Make sure that the file names are correct")
 		return
-	
+
 	# Call respective search functions
 	found = True
 	if(mode == "bfs"):
@@ -356,7 +355,7 @@ def main():
 		print "nodes expanded: " + str(nodesExpanded)
 		print "no solution found"
 		outputFO.write("nodes expanded: " + str(nodesExpanded) + "\n")
-		outputFO.write("no solution found" + "\n") 
+		outputFO.write("no solution found" + "\n")
 
 	# Close file
 	outputFO.close()
---
+++ Programming/Sentiment-Analysis/source.py
@@ -0,0 +1,131 @@
+import sys
+import string
+import re
+
+class NaiveBayes(object):
+    """docstring for NaiveBayes."""
+    def __init__(self):
+        super(NaiveBayes, self).__init__()
+        self.vocabulary = list()
+        self.features = list()
+
+    #############################################################
+    # Strips all punctuation, special characters, and numbers
+    #  and converts all letters to lowercase
+    #############################################################
+    def cleanSentence(self, line):
+        regEscapes = re.compile('[.!?@#$%^&*():;/,\\\<>\{\}\[\]+=\-_~`\'1234567890]')
+        return regEscapes.sub(r'', line.lower())
+
+    #############################################################
+    # Given a string, the string is stripped and converted to
+    #  lower case. Then each word is added to the vocabulary if
+    #  it is not already in it.
+    #############################################################
+    def __addToVocab(self, sentence):
+        # clean string then turn it into a list
+        cleanStr = self.cleanSentence(sentence).split()
+        for word in cleanStr:
+            if word not in self.vocabulary:
+                self.vocabulary.append(word)
+
+    #############################################################
+    # Given a file pointer, createVocab() adds each line to
+    #  the vocabulary
+    #############################################################
+    def createVocab(self, fp):
+        # make sure at beginning of file
+        fp.seek(0, 0)
+        for line in fp:
+            self.__addToVocab(line)
+
+    #############################################################
+    # Creates a features vector given a sentence
+    #  Goes through all words in the vocabulary, and if a vocabWord
+    #  is in the cleanStr then a 1 is placed in the index representing
+    #  that word in the feature vector
+    #############################################################
+    def createFeatureVector(self, sentence):
+        # get the classlabel before cleaning it. The classlabel is
+        #  the last character in the line, ignoring whitespace
+        classLabel = sentence.rsplit(None, 1)[-1]
+        # clean string then turn it into a list
+        cleanStr = self.cleanSentence(sentence).split()
+        newVector = [0] * len(self.vocabulary)
+        idx = 0
+        for vocabWord in self.vocabulary:
+            if vocabWord in cleanStr:
+                newVector[idx] = 1
+            idx += 1
+
+        # class label is the last element in feature vector
+        newVector.append(classLabel)
+        return newVector
+    #############################################################
+    # Given a file pointer, for each line in the file createFeatures()
+    #  adds converts it to a feature vector then adds it to the list of
+    #  feature vectores
+    #############################################################
+    def createFeatures(self, fp):
+        # make sure at beginning of file
+        fp.seek(0, 0)
+        self.vocabulary.sort()
+        for line in fp:
+            self.features.append(self.createFeatureVector(line))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#############################################################
+# The trainingSet.txt is in the following order:
+#       "some text    ", classlabel
+#   where classlabel is 1 is positive sentiment and 0 is
+#   negative sentiment.
+#
+# If cmmd line args are supplied, then use them. If not,
+#   then use the default ones
+#############################################################
+def main():
+    # If cmmd args are present adn there are the correct
+    #  correct number of them, get cmmd line args as files
+    args = sys.argv
+    if len(args) > 1:
+        if len(args) is not 3:
+            print("Lacking two cmmd line args: < trainingSet.txt > < testSet.txt >. \
+                    Either supply them or run with no arguments and program will use defualt sets")
+            return
+        # Open files
+        try:
+            trainingFO = open(args[1], "r")
+            testFO = open(args[2], "r")
+        except:
+            print("Error on opening a file. Make sure that the file names are correct")
+            return
+    else:
+        trainingFO = open("training_text.txt", "r")
+        testFO = open("test_text.txt", "r")
+
+    # Start Classifying
+    bayes = NaiveBayes()
+    bayes.createVocab(trainingFO)
+    bayes.createFeatures(trainingFO)
+
+
+
+    trainingFO.close()
+    testFO.close()
+
+if __name__ == "__main__":
+    main()
