9d53b44c8f2ca758b573c585025230dae6ec553d
fischjer4 <fischjer4@gmail.com>
2018-05-26 10:17:04 -0700

    Added classification. Works, but needs verified. Make sure that it is correct.

--- Programming/Sentiment-Analysis/source.py
+++ Programming/Sentiment-Analysis/source.py
@@ -1,8 +1,13 @@
 import sys
 import string
+import math
 import re
 
-
+#############################################################
+# Class to create bayes data. That is, the vocabulary and
+#  the feature vectors. This is done by sending a file object
+#  to createVocab(), and then to createFeatures()
+#############################################################
 class BayesData(object):
     """docstring for BayesData."""
     def __init__(self, vocabulary=None, features=None):
@@ -68,7 +73,7 @@ class BayesData(object):
     def createFeatureVector(self, sentence):
         # get the classlabel before cleaning it. The classlabel is
         #  the last character in the line, ignoring whitespace
-        classLabel = sentence.rsplit(None, 1)[-1]
+        classLabel = int(sentence.rsplit(None, 1)[-1])
         # clean string then turn it into a list
         cleanStr = self.cleanSentence(sentence).split()
         newVector = [0] * len(self.vocabulary)
@@ -95,19 +100,112 @@ class BayesData(object):
             self.features.append(self.createFeatureVector(line))
 
 
-class NaiveBayes(BayesData):
+
+
+
+
+#############################################################
+# Class is inherited from BayesData. It must be given a
+#  vocabulary which is a list of strings, and the
+#  trainedFeatures, which is a list of 1's and 0's where a 1
+#  means the vocabWord at vocab[i] is in the training document.
+# NaiveBayes creates a naive bayes net and the conditional
+#  probability tables based off of the trained features
+#############################################################
+class NaiveBayes(object):
     """docstring for NaiveBayes."""
-    def __init__(self, vocabulary, features):
-        super(NaiveBayes, self).__init__(vocabulary, features)
+    def __init__(self, vocabulary, trainedFeatures):
+        super(NaiveBayes, self).__init__()
+        self.vocabulary = vocabulary if vocabulary is not None else list()
+        self.features = trainedFeatures if trainedFeatures is not None else list()
+
+        # used to hold the number of features with classLabel X
+        self.classLabelSet = {0: 0, 1: 0}
+
+        # Objects where key is vocabWord and value is P(vocabWord | classLabel=1)
+        self.labelIsPositive = {}
+        self.labelIsNegative = {}
+
+        for vocabWord in self.vocabulary:
+            self.labelIsPositive[vocabWord] = {"isPresent": 0, "notPresent": 0}
+            self.labelIsNegative[vocabWord] = {"isPresent": 0, "notPresent": 0}
+
+        self.createPropabilities()
+
+    #############################################################
+    # Goes through all vocab words and finds the P(vocabWord | classLabel).
+    #  It does this via Uniform Dirichlet Priors. For classLabel=1
+    #  this is,
+    #
+    #  (#featureVectors with vocabword=1 AND classLabel=1) + 1
+    #  --------------------------------------------------------
+    #           (#featureVectors where classLabel=1) + 2
+    #############################################################
+    def createPropabilities(self):
+        for fVector in self.features:
+            # add one to the count for this class label
+            if fVector[-1] == 1 or fVector[-1] == 0:
+                self.classLabelSet[fVector[-1]] += 1
+
+            for idx in range(0, len(self.vocabulary)):
+                vocabWord = self.vocabulary[idx]
+                # If classLabel is 1
+                if fVector[-1] == 1:
+                    if fVector[idx] == 1:
+                        self.labelIsPositive[vocabWord]["isPresent"] += 1
+                    elif fVector[idx] == 0:
+                        self.labelIsPositive[vocabWord]["notPresent"] += 1
+                # If classLabel is 0
+                elif fVector[-1] == 0:
+                    if fVector[idx] == 1:
+                        self.labelIsNegative[vocabWord]["isPresent"] += 1
+                    elif fVector[idx] == 0:
+                        self.labelIsNegative[vocabWord]["notPresent"] += 1
+        # Now that we have the (#featureVectors with vocabword=1 AND classLabel=1)
+        #  and classLabelSet{} has (#featureVectors where classLabel=1),
+        #  add dichlet priors and compute P(vocabWord | classLabel)
+        for word, values in self.labelIsPositive.items():
+            values["isPresent"] = math.log2( (values["isPresent"] + 1) / (self.classLabelSet[1] + 2) )
+            values["notPresent"] = math.log2( (values["notPresent"] + 1) / (self.classLabelSet[1] + 2) )
+        for word, values in self.labelIsNegative.items():
+            values["isPresent"] = math.log2( (values["isPresent"] + 1) / (self.classLabelSet[0] + 2) )
+            values["notPresent"] = math.log2( (values["notPresent"] + 1) / (self.classLabelSet[0] + 2) )
 
 
 
+    #############################################################
+    # Given a feature vector, the function returns a tuple where
+    #  the first element is the probability the classLabel = 0,
+    #  and the second is the probability the classLabel = 1
+    #############################################################
+    def predictClassLabel(self, featureVector):
+        sumLabelPositive = 0
+        sumLabelNegative = 0
+        for idx in range(0, len(self.vocabulary)):
+            vocabWord = self.vocabulary[idx]
+            if featureVector[idx] == 1:
+                sumLabelPositive += self.labelIsPositive[vocabWord]["isPresent"]
+                sumLabelNegative += self.labelIsNegative[vocabWord]["isPresent"]
+            elif featureVector[idx] == 0:
+                sumLabelPositive += self.labelIsPositive[vocabWord]["notPresent"]
+                sumLabelNegative += self.labelIsNegative[vocabWord]["notPresent"]
 
+        totalVectors = self.classLabelSet[0] + self.classLabelSet[1]
+        probabilityPos = math.log2(self.classLabelSet[1]/totalVectors) + sumLabelPositive
+        probabilityNeg = math.log2(self.classLabelSet[0]/totalVectors) + sumLabelNegative
+        return [ probabilityNeg, probabilityPos ]
 
+    def predictAllTestFeatures(self, testFeatures):
+        numCorrectlyPredicted = 0
+        totalNumPredictions = len(testFeatures)
 
+        for featureVector in testFeatures:
+            result = self.predictClassLabel(featureVector)
+            prediction = 1 if result[1] > result[0] else 0
+            if prediction == featureVector[-1]:
+                numCorrectlyPredicted += 1
 
-
-
+        print("Accuracy: " + str((numCorrectlyPredicted / totalNumPredictions) * 100))
 
 
 
@@ -150,11 +248,13 @@ def main():
     trainingData.printToFile("preprocessed_train.txt")
 
     testData = BayesData(trainingData.vocabulary)
-    trainingData.createFeatures(testFO)
-    trainingData.printToFile("preprocessed_test.txt")
+    testData.createFeatures(testFO)
+    testData.printToFile("preprocessed_test.txt")
 
     # Start Classifying
-
+    bayes = NaiveBayes(trainingData.vocabulary, trainingData.features)
+    bayes.predictAllTestFeatures(trainingData.features)
+    bayes.predictAllTestFeatures(testData.features)
 
     trainingFO.close()
     testFO.close()
